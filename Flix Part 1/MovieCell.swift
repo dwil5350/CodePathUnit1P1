30afc03f5d1ca24bf5f217427fcd50c62cb27e21
dwil5350 <dwil5350@students.vsu.edu>
2022-09-14 19:44:53 -0400

    Added codepath unit 1 part 1 to repository

---
+++ Flix Part 1/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  Flix Part 1
+//
+//  Created by Denise Williams  on 9/14/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
